0984682efdc8635b7a4bd71d9fa28cebf80b03a9
Sean Norwood <norwood.sean@gmail.com>
2022-03-21 12:22:29 -0500

    chore: convert Layout to function component

--- src/components/FunctionLayout.js
+++ src/components/FunctionLayout.js
@@ -1,39 +1,107 @@
-import { useStaticQuery } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
+import { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import { useLocation } from '@reach/router';
 
-export const Layout = () => {
+import '../assets/sass/main.scss';
+
+const avatar = '../assets/img/avatar.png';
+
+export const FunctionLayout = ({ children }) => {
+  const [isPreloaded, setIsPreloaded] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsPreloaded(false);
+    });
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
+    query SiteTitleQueryTest {
       site {
         siteMetadata {
           title
+          description
+          siteUrl
+          og {
+            siteName
+            twitter
+          }
         }
       }
     }
   `);
 
+  const metas = [
+    {
+      name: 'description',
+      content: data.site.siteMetadata.description,
+    },
+    {
+      name: 'og:image',
+      content: avatar,
+    },
+    {
+      name: 'og:image:width',
+      content: `421px`,
+    },
+    {
+      name: 'og:image:height',
+      content: `421px`,
+    },
+    {
+      name: 'og:type',
+      content: 'website',
+    },
+    {
+      name: 'og:title',
+      content: data.site.siteMetadata.title,
+    },
+    {
+      name: 'og:description',
+      content: data.site.siteMetadata.description,
+    },
+    {
+      name: 'og:site_name',
+      content: data.site.siteMetadata.og.siteName,
+    },
+    {
+      name: 'og:url',
+      content: `${data.site?.siteMetadata?.siteUrl}${location.pathname}`,
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
+    {
+      name: 'twitter:description',
+      content: data.site.siteMetadata.description,
+    },
+    {
+      name: 'twitter:title',
+      content: data.site.siteMetadata.title,
+    },
+    {
+      name: 'twitter:image',
+      content: avatar,
+    },
+    {
+      name: 'twitter:creator',
+      content: data.site.siteMetadata.og.twitterCreator,
+    },
+  ];
+
   return (
     <>
-      <Helmet
-        title={data.site.siteMetadata.title}
-        meta={[
-          { name: 'description', content: 'Portfolio of Sean Norwood' },
-          { name: 'keywords', content: 'site, web' },
-        ]}
-      >
+      <Helmet title={data.site.siteMetadata.title}>
         <html lang="en" />
 
-        <meta property="og:url" content="https://stiforr.tech/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Sean Norwood" />
-        <meta property="og:description" content="Portfolio of Sean Norwood" />
-        <meta property="og:image" content="../assets/img/avatar.png" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="stiforr.tech" />
-        <meta property="twitter:url" content="https://stiforr.tech/" />
-        <meta name="twitter:title" content="Sean Norwood" />
-        <meta name="twitter:description" content="Portfolio of Sean Norwood" />
-        <meta name="twitter:image" content="../assets/img/avatar.png" />
+        {metas.map((meta) => (
+          <meta key={meta.name} name={meta.name} content={meta.content} />
+        ))}
       </Helmet>
       <div className={isPreloaded ? 'main-body is-preload' : 'main-body'}>
         {children}
